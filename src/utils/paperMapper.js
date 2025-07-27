39082a00a859cd39b1958aa4135a0b9f72433c3c
Mzwandile <mzwandileT21@gmail.com>
2025-07-27 18:36:06 +0800

    WORKING REMOTELY

--- src/utils/paperMapper.js
+++ src/utils/paperMapper.js
@@ -3,7 +3,7 @@ import EnglishFALP12020 from "../components/dashboardSidebarPages/quiz/Digitized
 
 // Map paper IDs directly to components
 const paperComponents = {
-    50: MathemathicsP1Nov2022Eng,       // ID 1 = Maths paper
+    156: MathemathicsP1Nov2022Eng,       // ID 1 = Maths paper
     17: EnglishFALP12020,     // ID 2 = English paper
     // Add more ID mappings as needed:
     // 3: PhysicsP1May2023,
