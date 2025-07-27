d40f0ee72a9243082221d109defb8c8a36b2ccc0
Mzwandile <mzwandileT21@gmail.com>
2025-07-27 17:53:38 +0800

    WORKING REMOTELY

--- src/utils/paperMapper.js
+++ src/utils/paperMapper.js
@@ -1,11 +1,14 @@
-//import MathemathicsP1Nov2022Eng from "../components/dashboardSidebarPages/quiz/DigitizedQuestionPapersComponents.js/maths/MathematicsP1Nov2022Eng";
+
 import EnglishFALP12020 from "../components/dashboardSidebarPages/quiz/DigitizedQuestionPapersComponents.js/EnglishFALP12020";
 import MathematicsP1Nov2022Eng
     from "../components/dashboardSidebarPages/quiz/DigitizedQuestionPapersComponents.js/maths/MathematicsP1Nov2022Eng";
+import MathematicsP2Nov2022Eng
+    from "../components/dashboardSidebarPages/quiz/DigitizedQuestionPapersComponents.js/maths/MathematicsP2Nov2022Eng";
 
 const paperComponents = {
     "Mathematics P1 Nov 2022 Eng": MathematicsP1Nov2022Eng,
-    "Mathematics P2 Nov 2022 Eng": EnglishFALP12020,
+    "Mathematics P2 Nov 2022 Eng": MathematicsP2Nov2022Eng,
+    "English P2 Nov 2022 Eng": EnglishFALP12020,
     // Add more mappings as needed
 };
 
